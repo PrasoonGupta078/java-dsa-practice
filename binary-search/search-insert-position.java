c1ec1a875fef67c6c3cf5128ed64dcef61ea0d66
Prasoon Gupta <129424171+PrasoonGupta078@users.noreply.github.com>
2026-04-21 22:30:41 +0530

    Update search-insert-position.java

--- binary-search/search-insert-position.java
+++ binary-search/search-insert-position.java
@@ -1,16 +1,29 @@
-int left = 0;
-int right = nums.length - 1;
+// Problem: Search Insert Position
+// Platform: LeetCode
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/search-insert-position/
+// Approach: Binary Search
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
 
-while (left <= right) {
-    int mid = left + (right - left) / 2;
+// Approach Explanation:
+// Use binary search to find the target.
+// If not found, return the position where it should be inserted.
 
-    if (nums[mid] == target) {
-        return mid; 
-    } else if (nums[mid] < target) {
-        left = mid + 1;
-    } else {
-        right = mid - 1;
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid; 
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
     }
 }
-
-return left;
